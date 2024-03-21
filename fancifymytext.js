340af677694eb93eb18649ed69db60f6828609bd
Dzmitry M <ma.dzmitry@gmail.com>
2024-03-21 14:00:40 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,18 +1,15 @@
 function makeBigger() {
-    alert("Making Bigger!");
     document.getElementById('textBox').style.fontSize = "24pt";
 };
 
 
 function makeFancyShmancy() {
-    alert("Making Fancy Shmancy, world!");
     document.getElementById('textBox').style.fontWeight = "bold";
     document.getElementById('textBox').style.textDecoration = "underline";
     document.getElementById('textBox').style.color = "blue";
 };
 
 function makeBoringBetty() {
-    alert("Making Boring Betty, world!");
     document.getElementById('textBox').style.fontWeight = "normal";
 
 };
@@ -21,7 +18,7 @@ function makeMoo() {
 	var str = document.getElementById("textBox").value
 	var parts = str.split(".");
 	str = parts.join("-Moo.");     
-    document.getElementById("textBox").value = str
+    	document.getElementById("textBox").value = str
 };
 
 
